4ee04f977ff2747817b52830f10e6ffd018a485d
Yana <kuziavra@gmail.com>
2018-11-12 18:20:27 -0600

    Output data in format that's immediately readable by bigtable
    print out metadata to command line

--- get-rep-history.py
+++ get-rep-history.py
@@ -1,14 +1,15 @@
 from stackapi import StackAPI
-import json
+import ndjson
 import APIkey
 
 SITE = StackAPI('stackoverflow', key=APIkey.key)
 SITE.max_pages = 300
 
-print SITE.page_size, SITE.max_pages
-
-
 rep = SITE.fetch('/users/1679187/reputation-history') # Some high-level user (past biggest privilege threshold)
 
+for field in rep:
+    if field != 'items':
+        print field, rep[field]
+
 with open('sample.json', 'w') as outfile:
-    json.dump(rep, outfile)
+    ndjson.dump(rep['items'], outfile)
